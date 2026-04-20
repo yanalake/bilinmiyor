f23814c32e66eba30da62a84c0f9ff87b6c91706
yanalake <163325029+yanalake@users.noreply.github.com>
2026-04-20 16:16:04 +0300

    Update liste.js

--- liste.js
+++ liste.js
@@ -1,6 +1,11 @@
 // Yeni eklediğin her şey en üste (başa) gelsin diye dizinin başına ekleme yapıyoruz.
 const sinemaNotlari = [
     {
+        baslik: "HE",
+        icerik: "Zaman, sinemada bükülebilen tek gerçektir. Nolan'ın kurgu oyunlarından Tarkovsky'nin mühürlenmiş zamanına bir yolculuk. <br><br> <img src='https://raw.githubusercontent.com/yanalake/bilinmiyor/main/arkaplan.png' style='width:100%; border-radius:5px; margin-top:10px;'>",
+        tarih: "20.04.2026"
+    }
+    {
         baslik: "SİNEMADA ZAMAN ALGISI",
         icerik: "Zaman, sinemada bükülebilen tek gerçektir. Nolan'ın kurgu oyunlarından Tarkovsky'nin mühürlenmiş zamanına bir yolculuk. <br><br> <img src='https://raw.githubusercontent.com/yanalake/bilinmiyor/main/arkaplan.png' style='width:100%; border-radius:5px; margin-top:10px;'>",
         tarih: "20.04.2026"
